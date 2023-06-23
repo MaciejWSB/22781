566d972721949d2c7351ca83df90dc7f74576c15
MaciejWSB <137516900+MaciejWSB@users.noreply.github.com>
2023-06-23 16:35:45 +0200

    Add files via upload

---
+++ kalkulator.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+print(add(5, 3))
+print(subtract(10, 4))
+print(multiply(2, 6))
+print(divide(8, 2))
