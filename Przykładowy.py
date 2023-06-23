aea361f8977ba541f730dae7be9eb4f79a181f9b
maciek Cgorzępa <maciej.jan.chorzepa@gmail.com>
2023-06-23 17:24:55 +0200

    Dodanie funkcji dzielenia

--- Przykładowy.py
+++ Przykładowy.py
@@ -7,5 +7,12 @@ def square(x):
 def multiply(x, y):
     return x * y
 
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Error: Nie można dzielić przez 0"
+
+print(divide(10, 2))
 print(multiply(5, 3))
 print(square(5))
